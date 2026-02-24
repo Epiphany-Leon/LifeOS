cad03f33a8389eb4aea7ecd5bf3e6a5233a9d8d6
Lihong_Gao <lihong_gao828@protonmail.com>
2026-02-24 16:25:18 -0500

    Initial Commit

---
+++ LifeOS/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LifeOS
+//
+//  Created by Lihong Gao on 2026-02-24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
